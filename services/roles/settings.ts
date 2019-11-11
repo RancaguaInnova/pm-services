5b768c2cadb6d22777645de2760c10db186a304e
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2019-11-11 16:38:13 -0300

    add validate

--- services/roles/settings.ts
+++ services/roles/settings.ts
@@ -1,102 +1,139 @@
-import { ServiceSettingSchema } from "moleculer";
+import { ServiceSettingSchema } from 'moleculer'
 
 const rolesSettings: ServiceSettingSchema = {
-  idField: "id",
+  idField: 'id',
   /*
-    * Public fields
-  */
+   * Public fields
+   */
   fields: [
-    "id",
-    "name",
-    "workplans",
-    "areas",
-    "lines",
-    "objectives",
-    "actions",
-    "activities",
-    "users",
-    "departments",
-    "roles",
+    'id',
+    'name',
+    'workplans',
+    'areas',
+    'lines',
+    'objectives',
+    'actions',
+    'activities',
+    'users',
+    'departments',
+    'roles',
   ],
 
   /*
-    * Validation schema
-  */
+   * Validation schema
+   */
   entityValidator: {
-    name: { type: "string", unique: true, empty: false },
-    workplans: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    areas: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    lines: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    objectives: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    actions: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    activities: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    users: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    departments: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
-    roles: { type: "object", empty: false, props: {
-      create: { type: "boolean" },
-      show: { type: "boolean" },
-      edit: { type: "boolean" },
-      delete: { type: "boolean" },
-      list: { type: "boolean" },
-      enabled: { type: "boolean" },
-    } },
+    name: { type: 'string', unique: true, empty: false },
+    workplans: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    areas: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    lines: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    objectives: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    actions: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+        validate: { type: 'boolean' },
+      },
+    },
+    activities: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    users: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    departments: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    roles: {
+      type: 'object',
+      empty: false,
+      props: {
+        create: { type: 'boolean' },
+        show: { type: 'boolean' },
+        edit: { type: 'boolean' },
+        delete: { type: 'boolean' },
+        list: { type: 'boolean' },
+        enabled: { type: 'boolean' },
+      },
+    },
   },
-};
+}
 
-export default rolesSettings;
+export default rolesSettings
