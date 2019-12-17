e5e3f4381b161fe41b1099f2b9bd1e16dfee2bc7
Joaquin Gumucio L <jjgumucio@gmail.com>
2019-12-17 10:21:50 -0300

    Makes transversality and approval optional

--- services/activities/settings.ts
+++ services/activities/settings.ts
@@ -3,8 +3,8 @@ import { ServiceSettingSchema } from "moleculer";
 const activitiesSettings: ServiceSettingSchema = {
   idField: "id",
   /*
-    * Public fields
-  */
+   * Public fields
+   */
   fields: [
     "id",
     "actionId",
@@ -26,36 +26,59 @@ const activitiesSettings: ServiceSettingSchema = {
   ],
 
   /*
-    * Validation schema
-  */
+   * Validation schema
+   */
   entityValidator: {
     actionId: { type: "string", empty: false },
     name: { type: "string", unique: true, empty: false },
     description: { type: "string", empty: false },
-    status: { type: "enum", values: [ "not-started", "in-progress", "finished" ], empty: false },
+    status: { type: "enum", values: ["not-started", "in-progress", "finished"], empty: false },
     responsibleId: { type: "string", empty: false },
-    executedFunds: { type: "array", items: { type: "object", props: {
-      source: { type: "string", empty: false },
-      amount: { type: "number", positive: true, integer: true },
-    } } },
+    executedFunds: {
+      type: "array",
+      items: {
+        type: "object",
+        props: {
+          source: { type: "string", empty: false },
+          amount: { type: "number", positive: true, integer: true },
+        },
+      },
+    },
     coordinatedWith: { type: "array", items: "string" },
-    beneficiaries: { type: "array", items: { type: "object", props: {
-      description: { type: "string", empty: false },
-      quantity: { type: "number", positive: true, integer: true },
-    } } },
-    location: { type: "object", optional: true, props: {
-      name: { type: "string", empty: false },
-      lat: { type: "number", positive: true, integer: true },
-      lng: { type: "number", positive: true, integer: true },
-    } },
+    beneficiaries: {
+      type: "array",
+      items: {
+        type: "object",
+        props: {
+          description: { type: "string", empty: false },
+          quantity: { type: "number", positive: true, integer: true },
+        },
+      },
+    },
+    location: {
+      type: "object",
+      optional: true,
+      props: {
+        name: { type: "string", empty: false },
+        lat: { type: "number", positive: true, integer: true },
+        lng: { type: "number", positive: true, integer: true },
+      },
+    },
     comments: { type: "string", optional: true },
-    transversality: { type: "array", optional: true, items: { type: "object", props: {
-      areaId: { type: "string", empty: false },
-      lineId: { type: "string", empty: false },
-      objectiveId: { type: "string", empty: false },
-      actionId: { type: "string", empty: false },
-    } } },
-    approved: { type: "boolean" },
+    transversality: {
+      type: "array",
+      optional: true,
+      items: {
+        type: "object",
+        props: {
+          areaId: { type: "string", empty: false, optional: true },
+          lineId: { type: "string", empty: false, optional: true },
+          objectiveId: { type: "string", empty: false, optional: true },
+          actionId: { type: "string", empty: false, optional: true },
+        },
+      },
+    },
+    approved: { type: "boolean", optional: true },
   },
 };
 
--- services/users.service.ts
+++ services/users.service.ts
@@ -12,81 +12,76 @@ import findByQuery from "./users/actions/findByQuery";
 import updateForgotToken from "./users/actions/updateForgotToken";
 
 // Methods:
-import generateEntity from "./users/methods/generateEntity"
+import generateEntity from "./users/methods/generateEntity";
 import hashPassword from "./users/methods/hashPassword";
 import insertUser from "./users/methods/insertUser";
 import sendAccountValidationEmail from "./users/methods/sendAccountValidationEmail";
 import userExists from "./users/methods/userExists";
 
 const UsersService: ServiceSchema = {
-	name: "users",
-	version: 1,
+  name: "users",
+  version: 1,
 
-	mixins: [
-		DbService(process.env.MONGO_URI, "users"),
-		Hooks,
-	],
+  mixins: [DbService(process.env.MONGO_URI, "users"), Hooks],
 
-	/**
-	 * Service settings
-	 */
-	settings,
+  /**
+   * Service settings
+   */
+  settings,
 
-	/**
-	 * Service dependencies
-	 */
-	dependencies: [],
+  /**
+   * Service dependencies
+   */
+  dependencies: [],
 
-	// hooks: {
-	// 	before: {
-	// 		list: ["checkIsAuthenticated", "checkUserRole"],
-	// 		get: ["checkIsAuthenticated", "checkUserRole"],
-	// 		update: ["checkIsAuthenticated", "checkUserRole"],
-	// 		remove: ["checkIsAuthenticated", "checkUserRole"]
-	// 	}
+  // hooks: {
+  // 	before: {
+  // 		list: ["checkIsAuthenticated", "checkUserRole"],
+  // 		get: ["checkIsAuthenticated", "checkUserRole"],
+  // 		update: ["checkIsAuthenticated", "checkUserRole"],
+  // 		remove: ["checkIsAuthenticated", "checkUserRole"]
+  // 	}
 
-	// },
-	/**
-	 * Actions
-	 */
-	actions: {
-		create,
-		findByQuery,
-		updateForgotToken,
-	},
+  // },
+  /**
+   * Actions
+   */
+  actions: {
+    create,
+    findByQuery,
+    updateForgotToken,
+  },
 
-	/**
-	 * Events
-	 */
-	events: {
+  /**
+   * Events
+   */
+  events: {},
 
-	},
+  /**
+   * Service private methods
+   */
+  methods: {
+    generateEntity,
+    hashPassword,
+    insertUser,
+    sendAccountValidationEmail,
+    userExists,
+  },
 
-	/**
-	 * Service private methods
-	 */
-	methods: {
-		generateEntity,
-		hashPassword,
-		insertUser,
-		sendAccountValidationEmail,
-		userExists,
-	},
+  /**
+   * Service created lifecycle event handler
+   */
+  created() {},
 
-	/**
-	 * Service created lifecycle event handler
-	 */
-	created() {},
+  /**
+   * Service started lifecycle event handler
+   */
+  async started() {},
 
-	/**
-	 * Service started lifecycle event handler
-	 */
-	async started() {},
-
-	/**
-	 * Service stopped lifecycle event handler
-	 */
-	async stopped() {},
+  /**
+   * Service stopped lifecycle event handler
+   */
+  async stopped() {},
 };
 
 export = UsersService;
